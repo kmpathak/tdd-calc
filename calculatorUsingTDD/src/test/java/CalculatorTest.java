c83f8ca9abc1fb8bfc3dbd788fc774cf16bc9dcb
murti <murtikrishna58@gmail.com>
2022-05-02 15:39:12 +0530

    Test case passed on - number with an empty string

--- calculatorUsingTDD/src/test/java/CalculatorTest.java
+++ calculatorUsingTDD/src/test/java/CalculatorTest.java
@@ -1,15 +1,16 @@
-import org.junit.After;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
 
 public class CalculatorTest {
 
     int Add(String numbers) {
         return 0;
+    }
+
+    @Test
+    public void additionTest() {
+        Assert.assertEquals(0, Add(""));
     }
 
 }
