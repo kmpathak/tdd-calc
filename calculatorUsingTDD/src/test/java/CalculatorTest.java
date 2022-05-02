41b609be97623269dd41f35829f50c4ce936adbb
murti <murtikrishna58@gmail.com>
2022-05-02 15:35:12 +0530

    class named 'CalculatorTest' is created with method signature Add(String numbers).

---
+++ calculatorUsingTDD/src/test/java/CalculatorTest.java
@@ -0,0 +1,15 @@
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+public class CalculatorTest {
+
+    int Add(String numbers) {
+        return 0;
+    }
+
+}
