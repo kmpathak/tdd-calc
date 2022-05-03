a2b9eca8e12d83b910ddafdd0e39f07a76c7a7bc
murti <murtikrishna58@gmail.com>
2022-05-03 17:45:48 +0530

    Test case failed - failed on string argument with single number

--- calculatorUsingTDD/src/test/java/CalculatorTest.java
+++ calculatorUsingTDD/src/test/java/CalculatorTest.java
@@ -5,12 +5,13 @@ import org.junit.Test;
 public class CalculatorTest {
 
     int Add(String numbers) {
-        return 0;
+            return 0;
     }
 
     @Test
     public void additionTest() {
         Assert.assertEquals(0, Add(""));
+        Assert.assertEquals(1, Add("1"));
     }
 
 }
