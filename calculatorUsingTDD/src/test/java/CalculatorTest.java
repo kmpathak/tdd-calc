54e25bd5e7f24b863faf7f48cade536290283da4
murti <murtikrishna58@gmail.com>
2022-05-03 17:48:41 +0530

    Test case passed - allowing empty string as well as single  numbers in the string argument.

--- calculatorUsingTDD/src/test/java/CalculatorTest.java
+++ calculatorUsingTDD/src/test/java/CalculatorTest.java
@@ -5,13 +5,18 @@ import org.junit.Test;
 public class CalculatorTest {
 
     int Add(String numbers) {
+        if(numbers.length()==0)
             return 0;
+        else{
+            return Integer.parseInt(numbers);
+        }
     }
 
     @Test
     public void additionTest() {
         Assert.assertEquals(0, Add(""));
         Assert.assertEquals(1, Add("1"));
+        Assert.assertEquals(762, Add("762"));
     }
 
 }
